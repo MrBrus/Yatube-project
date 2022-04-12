81d2c7a27f489e63faa41de4ca9909fc994188c4
Vlad Brusensky <brusen_sky@mail.ru>
2022-04-13 00:39:45 +0300

    Спринт 6 Первая отправка

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -11,13 +11,11 @@ urlpatterns = [
     path('follow/', views.follow_index, name='follow'),
     path('profile/<str:username>/follow/',
          views.profile_follow,
-         name='profile_follow'
-         ),
+         name='profile_follow'),
     path(
         'profile/<str:username>/unfollow/',
         views.profile_unfollow,
-        name='profile_unfollow'
-        ),
+        name='profile_unfollow'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
